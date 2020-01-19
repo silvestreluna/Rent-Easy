4d32cdd9db7b3bbb3feebdade8c82dcdc03fcf6b
Silvestre Luna <slvtrluna@gmail.com>
2020-01-19 11:40:53 -0600

    completed file structure and have a working ui page

--- rent-easy.ui/src/App/App.js
+++ rent-easy.ui/src/App/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.scss';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Button variant="contained" color="primary">
+        click me
+      </Button>
     </div>
   );
 }
